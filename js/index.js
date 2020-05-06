451b87453514174434e39de1d230078b30b71990
sesp13 <sespinosarte99@gmail.com>
2020-05-05 21:24:06 -0500

    reestructuración del archivos de javascript

--- js/index.js
+++ js/index.js
@@ -1,8 +1,3 @@
 $(document).ready(function () {
-    //Esperar 1 minuto para ocultar a los volados de la partida
-    window.setTimeout(function () { ocultarVolados() }, 60000);
 
-    function ocultarVolados() {
-        $('#volados').hide();
-    }
 });
--- js/juego.js
+++ js/juego.js
@@ -10,3 +10,12 @@ $('#eliminar-jugador').click(function () {
     $('#formulario-eliminar').show();
     $(this).hide();
 });
+
+$(document).ready(function () {
+    //Esperar 1 minuto para ocultar a los volados de la partida
+    window.setTimeout(function () { ocultarVolados() }, 60000);
+
+    function ocultarVolados() {
+        $('#volados').hide();
+    }
+});
